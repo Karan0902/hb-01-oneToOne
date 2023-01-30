89be5d06f544747be420cc8139d43926db510ce5
Karan Motiramani <karan0902@yahoo.co.in>
2023-01-30 21:07:20 +0530

    * add comment

--- src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
+++ src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
@@ -26,6 +26,7 @@ public class GetInstructorCoursesDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
+//			add comment
 //			start a transaction
 			session.beginTransaction();
 			
